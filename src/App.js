47b9a13f84427fe1b7ed65436870f7b35d9c5372
Oluwasanmi Idowu <idowuoluwasanmi@gmail.com>
2022-07-04 15:40:49 +0100

    ncc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import ErrorPage from "./ErrorPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route path="/create" element={<Create />} />
+                        <Route path="/blogs/:id" element={<BlogDetails />} />
+                        <Route path="*" element={<ErrorPage />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
+
 
 export default App;
